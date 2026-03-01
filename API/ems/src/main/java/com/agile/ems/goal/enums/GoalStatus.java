3b2507a5e5dfb237dbf31ee7bd92200f673a894b
tbm02 <tarun989898@gmail.com>
2026-03-01 15:41:18 +0530

    API:feat: Added goalstatus and quarter enums.

---
+++ API/ems/src/main/java/com/agile/ems/goal/enums/GoalStatus.java
@@ -0,0 +1,8 @@
+package com.agile.ems.goal.enums;
+
+public enum GoalStatus {
+    DRAFT,
+    SUBMITTED,
+    APPROVED,
+    REJECTED
+}
---
+++ API/ems/src/main/java/com/agile/ems/goal/enums/Quarter.java
@@ -0,0 +1,5 @@
+package com.agile.ems.goal.enums;
+
+public enum Quarter {
+    Q1, Q2, Q3, Q4
+}
