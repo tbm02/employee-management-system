6a7eb97c81530da1a486250766888085b2ca5cdc
tbm02 <tarun989898@gmail.com>
2026-03-01 15:38:31 +0530

    API:fix: removed the ROLE_ from SecurityConfig.java

--- API/ems/src/main/java/com/agile/ems/auth/security/SecurityConfig.java
+++ API/ems/src/main/java/com/agile/ems/auth/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/users").hasAnyRole("ROLE_ADMIN","ROLE_HR")
+                        .requestMatchers(HttpMethod.POST, "/api/users").hasAnyRole("ADMIN","HR")
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
